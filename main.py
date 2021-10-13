0a9affc218fee8fcd7c3e5eb8e154ff042c82996
ben-pharma <ben.groves@pharmagenesis.com>
2021-10-13 16:28:48 +0100

    'Close', 'Volume', 'Dividends', 'Sto

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import requests
-import json
-import pandas as pd
-import numpy as np
+import yfinance as yf
 import matplotlib.pyplot as plt
-import seaborn as sns
-import cryptocompare as cc
+import streamlit as st
 
+btc = yf.Ticker("BTC-USD")
+df = btc.history(start = "2020-01-01", end = "2020-06-01")
+
+print(df.columns)
+
+
+def open_close_plot (data):
+    plt.plot(data['Open'], "g", linestyle = "-", linewidth = 1.5)
+    plt.plot(data['Close'], "r", linestyle = "-", linewidth = 1.5)
+    plt.legend(['Open', 'Close'])
+    return plt.show()
+
+
+open_close_plot(df)
